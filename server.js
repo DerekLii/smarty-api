e1beb489aa110a58da065bb399d07d67bc7c10a6
Derek Li <derekli33@hotmail.com>
2022-01-18 09:13:38 -0500

    change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt-nodejs";
 import cors from "cors";
 import knex from "knex";
 
-import {handleSignin} from "./controllers/signin.js";
+import { handleSignin } from "./controllers/signin.js";
 import { handleRegister } from "./controllers/register.js";
 import { handleProfileGet } from "./controllers/profile.js";
 import { handleImage, handleApiCall } from "./controllers/image.js";
@@ -25,27 +25,27 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send('success');
+  res.send("success");
 });
 
-app.post("/signin", (req,res) => {
-  handleSignin(req,res,db,bcrypt);
+app.post("/signin", (req, res) => {
+  handleSignin(req, res, db, bcrypt);
 });
 
-app.post('/register', (req,res) => {
-  handleRegister(req,res,db,bcrypt);
+app.post("/register", (req, res) => {
+  handleRegister(req, res, db, bcrypt);
 });
 
-app.get("/profile/:id", (req,res) => {
-  handleProfileGet(req,res,db);
+app.get("/profile/:id", (req, res) => {
+  handleProfileGet(req, res, db);
 });
 
-app.put("/image", (req,res) => {
-  handleImage(req,res,db);
+app.put("/image", (req, res) => {
+  handleImage(req, res, db);
 });
 
-app.post("/imageurl", (req,res) => {
-  handleApiCall(req,res);
+app.post("/imageurl", (req, res) => {
+  handleApiCall(req, res);
 });
 
 app.listen(3000, () => {
