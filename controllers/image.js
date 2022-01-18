d4566dbd3d704803d38818a70575843bedfa5f35
Derek Li <derekli33@hotmail.com>
2022-01-18 08:42:47 -0500

    adding

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+import Clarifai from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "10a8b821884748c995402013e13f12c2",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with api"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+export { handleImage, handleApiCall };
